49adf7e1bd4f0dbc58f083d6e5f7f0db9c4a1315
masumbillah99 <masumbillahyusuf1621@gmail.com>
2026-08-11 14:51:26 +0600

    block access to other users data and return a 403 forbidden response

--- demo/controllers/single-note.php
+++ demo/controllers/single-note.php
@@ -5,6 +5,20 @@ $db = new Database($config['database']);
 
 $pageHeading = "Note Details";
 
-$note = $db->query("select * from notes where id = :id", [':id' => $_GET['id']])->fetch();
+$note = $db->query("select * from notes where id = :id", [
+    ':id' => $_GET['id']
+])->fetch();
+
+
+if (!$note) {
+    abort();
+}
+
+$currentUserId = 1;
+
+
+if ($note['user_id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 require "views/single-note.view.php";
---
+++ demo/views/403.php
@@ -0,0 +1,22 @@
+<?php require("partials/header.php") ?>
+
+
+<!-- page content starts here -->
+
+<div class="min-h-full">
+    <?php require("partials/nav.php") ?>
+
+    <main class="">
+        <div class="mx-auto max-w-7xl px-4 py-9 sm:px-6 lg:px-8">
+            <h3 class="text-2xl font-bold">Sorry, you are not authorized to view this page.</h3>
+            <p class="mt-4">
+                <a href="/" class="text-blue underline">Go back home</a>
+            </p>
+        </div>
+    </main>
+
+</div>
+
+<!-- page content ends here -->
+
+<?php require("partials/footer.php") ?>
